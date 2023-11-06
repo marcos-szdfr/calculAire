a83315a9f818bb313f2b2603746eab6e9903d1ef
marcos-szdfr <marcos.szdfr@eduge.ch>
2023-11-06 15:02:37 +0100

    first commit

---
+++ php/fonctions.php
@@ -0,0 +1,11 @@
+<?php
+
+$formes = filter_input(INPUT_POST, "formes");
+
+switch ($formes) {
+    case 'Carre': 
+        echo '<input type="text" name="" id="">';
+        break;
+    case 'Rectangle':
+    
+}
