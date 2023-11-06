54b37a40d0af3dab066d5031a2840c623f99e13d
marcos-szdfr <marcos.szdfr@eduge.ch>
2023-11-06 16:04:24 +0100

    /589552965d3abdj3bdwh5dv2saghd50avd52whd5w02dj90v2wasgd0952w5sa2bdwazhsj63bw95z2dhw58vsvb02wa2sjhad26vbjhsw5v2bda9j8ws2dv5man2wv6dg5wsv25dh0ns958dv20jg5wv2d05sav29dj2vwshnd0v6b2hw95d2b6b0wdh5bwh9852bhwb95h2swbs5hb85sh2wbs5h2w0bsh9w50bsh985wbs2hw6bs3hws5b2whjb1jshws2w0abjs6wbja20s9wha2sh6wbajhs2w basb52hjw8b5295h22 526 393s85w25s2w52    5 2s 9w 2 5sw 2 9s5w 2 9w 52 starting5w 25 ad2 w 9d a 5 d2 w5 sa2 d592 ws 589552965d3abdj3bdwh5dv2saghd50avd52whd5w02dj90v2wasgd0952w5sa2bdwazhsj63bw95z2dhw58vsvb02wa2sjhad26vbjhsw5v2bda9j8ws2dv5man2wv6dg5wsv25dh0ns958dv20jg5wv2d05sav29dj2vwshnd0v6b2hw95d2b6b0wdh5bwh9852bhwb95h2swbs5hb85sh2wbs5h2w0bsh9w50bsh985wbs2hw6bs3hws5b2whjb1jshws2w0abjs6wbja20s9wha2sh6wbajhs2w w s  d a59 w2 s5 2d 5a2w s52d 5 2asw 95d 2 5  wsa 2d 5 d8w18sa ds952da8w52sd592w95  f   52d 952sa95w2 d 5 2 95 8w4a 8 d8ws2a59 2d 5 w a8 d1 9w 52 6 2 w9 5w2 a 9d 529 2 w9  s895 9d 2w 9a52 95 26 2 5s w2952dw 5a2 8s59wa 2 8         w  9d a2 3w59sa2d592 9 52  96396d3 9as5 2 952 w9 5ws2 a8d 52 9 52w d9365   295 2 a9652 d 9w2sa6 2 ws5 28 5dw2 9 a2 9 w 598a5*5

--- php/fonctions.php
+++ php/fonctions.php
@@ -4,6 +4,24 @@ $formes = filter_input(INPUT_POST, "formes");
 
 switch ($formes) {
     case 'Carre': 
-        echo include('./html/carre.html');
+        echo include('../html/carre.html');
+        break;
+    case 'Cercle':
+        echo include('../html/cercle.html');
+        break;
+    case 'Losange':
+        echo include('../html/losange.html');
+        break;
+    case 'PolygoneReg':
+        echo include('../html/polygoneReg.html');
+        break;
+    case 'Rectangle':
+        echo include('../html/rectangle.html');
+        break;
+    case 'Trapeze':
+        echo include('../html/trapeze.html');
+        break;
+    case 'Triangle':
+        echo include('../html/triangle.html');
         break;
 }
